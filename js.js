0972602b30560adfa2e12ac397f480b1cba194a3
Patric-Laptop <burgherrp@bzz.ch>
2018-06-21 09:36:14 +0200

    and another one

--- js.js
+++ js.js
@@ -31,35 +31,41 @@ function callMethods() {
 }
 
 function checkLogin() {
-    console.log("try to check");
-
-    var entUsername = "48414c4c4f";
-    var entPw = "39393838313132323333";
-
-    var newU = CryptoJS.AES.encrypt(document.getElementById("inputText").value, "Super Secret Key");
-    var newP = CryptoJS.AES.encrypt(document.getElementById("inputPw").value, "Super Secret Key");
-    var decryptedU = CryptoJS.AES.decrypt(newU, "Super Secret Key");
-    var decryptedP = CryptoJS.AES.decrypt(newP, "Super Secret Key");
-
-    if (a) {
-        if (entUsername == decryptedU && entPw == decryptedP) {
-            document.getElementById("message").style.color = "green";
-            document.getElementById("message").innerHTML = "Success";
-        } else {
-            grecaptcha.reset();
-            document.getElementById("submitButton").disabled = true;
-            document.getElementById("submitButton").style.visibility = "hidden";
-
-            document.getElementById("message").style.color = "red";
-            document.getElementById("message").innerHTML = "Try again / try:" + tries;
-            a = false;
-            setTimeout(function () {
-                a = true;
-                tries++;
-            }, 5000);
-        }
-    } else {
+    if (grecaptcha.getResponse().length == 0) {
+        //reCaptcha not verified
         document.getElementById("message").style.color = "red";
-        document.getElementById("message").innerHTML = "Try harder !!";
+        document.getElementById("message").innerHTML = "do the reCaptcha !!!";
+    } else {
+        console.log("try to check");
+
+        var entUsername = "48414c4c4f";
+        var entPw = "39393838313132323333";
+
+        var newU = CryptoJS.AES.encrypt(document.getElementById("inputText").value, "Super Secret Key");
+        var newP = CryptoJS.AES.encrypt(document.getElementById("inputPw").value, "Super Secret Key");
+        var decryptedU = CryptoJS.AES.decrypt(newU, "Super Secret Key");
+        var decryptedP = CryptoJS.AES.decrypt(newP, "Super Secret Key");
+
+        if (a) {
+            if (entUsername == decryptedU && entPw == decryptedP) {
+                document.getElementById("message").style.color = "green";
+                document.getElementById("message").innerHTML = "Success";
+            } else {
+                grecaptcha.reset();
+                document.getElementById("submitButton").disabled = true;
+                document.getElementById("submitButton").style.visibility = "hidden";
+
+                document.getElementById("message").style.color = "red";
+                document.getElementById("message").innerHTML = "Try again / try:" + tries;
+                a = false;
+                setTimeout(function () {
+                    a = true;
+                    tries++;
+                }, 5000);
+            }
+        } else {
+            document.getElementById("message").style.color = "red";
+            document.getElementById("message").innerHTML = "Try harder !!";
+        }
     }
 }
